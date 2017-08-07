849adbe21f1d718d8b34b202330950fd5c60325b
Alex <alex@brewingcode.net>
2017-08-07 16:35:33 -0700

    Give Foundation CSS a try
    Note that we are using a fork of a fork:
    
    https://github.com/akre54/foundation/commit/a958cd3ef43db77d8a5e1696134c4fbf42bf330b

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,6 +6,11 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
+    stylusOptions: {
+      includePaths: [
+        'bower_components/foundation/stylus'
+      ]
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
